976990ae3d7ea7b5f284b21010f557d396b8fc89
faraimoyo <farai.moyo@yahoo.com>
2023-03-11 22:42:27 +0000

    Add even listener to all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,13 @@ const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
-const chooseWisely = [];
+const chooseWisely = ["rock", "paper", "scissors", "lizard", "spock"];
+/**
+ * Add event listener to all buttons
+ */
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
