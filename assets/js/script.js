cd8128631cf6487fcab99156fd00bd8f5b1fc3bf
faraimoyo <farai.moyo@yahoo.com>
2023-03-11 23:01:14 +0000

    Te main game function that accepts one selection which is the data choice value of the selected button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
-const chooseWisely = ["rock", "paper", "scissors", "lizard", "spock"];
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 /**
  * Add event listener to all buttons
  */
@@ -17,4 +17,20 @@ for (let button of buttons) {
         let playerChoice = this.getAttribute("data-choice");
         playGame(playerChoice);
     });
+}
+/**
+ * The main game function that accepts one selection
+ * which is the data choice value of the selected button
+ */
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assest/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+    updateScores(result);
 }
