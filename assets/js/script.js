2c043de6f7709554cef6f8f9024f560a47bfa747
faraimoyo <farai.moyo@yahoo.com>
2023-03-11 22:28:00 +0000

    Declare constants for DOM elements and possible choices

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Declare constants for DOM elements
+ * and possible choices
+ */
+const buttons = document.getElementsByClassName("control");
+const playerScore =document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const chooseWisely = [];
